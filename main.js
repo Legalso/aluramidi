bb15e5e5d541a42ab69bd2aa4f250926c0b8703f
caiopatriota <caiopatriotabarros@gmail.com>
2023-03-23 00:00:43 -0300

    Som funcional

--- main.js
+++ main.js
@@ -7,8 +7,16 @@ const listaDeTeclas = document.querySelectorAll('.tecla');
 let contador = 0;
 
 while (contador < listaDeTeclas.length) {
-    listaDeTeclas[contador].onclick = function () {
-        tocaSom('')
+
+    const tecla = listaDeTeclas[contador];
+
+    const instrumento = tecla.classList[1];
+
+    //template string
+    const idAudio = `#som_${instrumento}`;
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
     }
     contador = contador + 1;
 }
